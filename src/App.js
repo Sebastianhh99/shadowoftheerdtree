36a5dc6f98a40f6a6368f6d1815605805f7b7490
Sebastian Herrera <sebastianhh1999@gmail.com>
2024-06-17 21:19:11 -0500

    eldenring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import emax from "./xd.png";
+import eldenring from "./eldenring.png";
 import Countdown from "react-countdown";
 
 function App() {
   return (
     <div className="app">
-      <img src={emax} className="app__emax" alt="emax" />
+      <img src={eldenring} className="app__emax" alt="emax" />
       <h1 className="app__counter">
         <Countdown date={new Date(2024, 5, 20, 17)} />
       </h1>
